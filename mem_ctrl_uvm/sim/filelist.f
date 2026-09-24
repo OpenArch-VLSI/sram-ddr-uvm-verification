f666c27b348136335378aae6777fb417fe35c89c
DSSrijith <srijith.sudhir@gmail.com>
2026-09-24 20:23:51 +0530

    Add simulation filelist in compile order

---
+++ mem_ctrl_uvm/sim/filelist.f
@@ -0,0 +1,8 @@
+../rtl/mem_ctrl_pkg.sv
+../rtl/bank_fsm.sv
+../rtl/mem_ctrl.sv
+../tb/mem_ctrl_if.sv
+../tb/mem_ctrl_assertions.sv
++incdir+../tb/uvm
+../tb/uvm/mem_ctrl_pkg_tb.sv
+../tb_top/tb_top.sv
